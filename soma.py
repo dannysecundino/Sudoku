0d148ce61a42931c2d010da88ab4b8554559d7c5
Guilimartins <jguilimartins@gmail.com>
2024-09-03 19:28:14 -0300

    print sub

--- soma.py
+++ soma.py
@@ -3,7 +3,9 @@ b = int(input())    # recebe o segundo numero
 operador = input()
 
 if operador == '+': #todo: implementar a soma
-    s = a + b
+    s = a+b
 elif operador == '-': #todo: implementar a subtracao
     s = a-b
     print("danny tem o mel")
+
+print(s)
