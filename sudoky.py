ccad8c26316f7ba6ff4b59e472c8a7a90db8291e
Guilimartins <jguilimartins@gmail.com>
2024-09-04 18:16:43 -0300

    Visualização Montada!

--- sudoky.py
+++ sudoky.py
@@ -6,11 +6,15 @@ jogo = [[" ", " ", " ", " ", " ", " ", " ", " ", " "],
         [" ", " ", " ", " ", " ", " ", " ", " ", " "],
         [" ", " ", " ", " ", " ", " ", " ", " ", " "],
         [" ", " ", " ", " ", " ", " ", " ", " ", " "],
-        [" ", " ", " ", " ", " ", " ", " ", " ", " "]] #Essa "matriz" representara o jogo por completo. Nela,serao adicionadas aas pistas e as jogadas do usuario
+        [" ", " ", " ", " ", " ", " ", " ", " ", " "]] #Essa "matriz" representara o jogo por completo. Nela,serao adicionadas as pistas e as jogadas do usuario
+    
+print("    A   B   C    D   E   F    G   H   I")                #Iniciando a visualização do sudoku
+for i in range(9):                                              #Vai percorrer as 9 linhas (0,9]
+    if i == 3 or i == 6:                                        #Nas linhas após o a linha "3" e a linha "6" o padrão de linha muda de "+" e "-" para "+" e "="
+        print(" ++===+===+===++===+===+===++===+===+===++ ")    #Linhas com "+" e "=""
+    else:                                                       
+        print(" ++---+---+---++---+---+---++---+---+---++ ")    #Linhas mais comuns
+    print(f"{i+1}|| {jogo[i][0]} | {jogo[i][1]} | {jogo[i][2]} || {jogo[i][3]} | {jogo[i][4]} | {jogo[i][5]} || {jogo[i][6]} | {jogo[i][7]} | {jogo[i][8]} ||{i+1}")
+print(" ++---+---+---++---+---+---++---+---+---++ ")            
+print("    A   B   C    D   E   F    G   H   I")                #Finalização da visualização do sudoku
 
-print("   A B C D E F G H I   ")
-print(" ++---+---+---++---+---+---++---+---+---++ ")
-
-for i in range(0, 9):
-    for j in range(0, 9):
-        
