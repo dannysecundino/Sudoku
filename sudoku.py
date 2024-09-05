88ef8bc907171cab78c02228f4b88ca3053acc53
Guilimartins <jguilimartins@gmail.com>
2024-09-05 20:30:59 -0300

    enviando pro danny

--- sudoku.py
+++ sudoku.py
@@ -4,7 +4,7 @@ arquivos = sys.argv                         # Esta lendo as informações dadas 
 arquivo = open(arquivos[1], "r")            # Esta abrindo o arquivo com o nome dado no terminal
 linhas = arquivo.readlines()        
 for coordenadas in linhas:                  #Estamos percorrendo todas as linhas do arquivo aberto
-    
-    
+
+#teste    
 
 #acao = str(input("\n            Digite sua entrada:"))                                                                                            
